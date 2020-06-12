9bb9d8ba8ac04787ade3ded1c5ec05b7ff28ad47
Tony Seebregts <tony.seebregts@gmail.com>
2020-06-12 13:06:57 -0700

    Relaxed constraint on column order

--- acl/tsv.go
+++ acl/tsv.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"regexp"
 	"strings"
+	"time"
 
 	"google.golang.org/api/sheets/v4"
 )
@@ -15,11 +16,39 @@ func MakeTSV(f io.Writer, data *sheets.ValueRange) error {
 		return fmt.Errorf("Empty sheet")
 	}
 
-	// ... header
+	// .. build index
+	index := map[string]int{}
 	row := data.Values[0]
-	header := make([]string, len(row))
 	for i, v := range row {
-		header[i] = clean(v.(string))
+		k := normalise(v.(string))
+		if _, ok := index[k]; ok {
+			return fmt.Errorf("Duplicate column name '%s'", v.(string))
+		}
+
+		index[k] = i
+	}
+
+	// ... header
+	row = data.Values[0]
+	header := []string{}
+
+	if ix, ok := index["cardnumber"]; ok {
+		header = append(header, clean(row[ix].(string)))
+	}
+
+	if ix, ok := index["from"]; ok {
+		header = append(header, clean(row[ix].(string)))
+	}
+
+	if ix, ok := index["to"]; ok {
+		header = append(header, clean(row[ix].(string)))
+	}
+
+	for _, v := range row {
+		k := normalise(v.(string))
+		if k != "cardnumber" && k != "from" && k != "to" {
+			header = append(header, clean(v.(string)))
+		}
 	}
 
 	if len(header) == 0 {
@@ -41,16 +70,35 @@ func MakeTSV(f io.Writer, data *sheets.ValueRange) error {
 	// ... records
 	records := [][]string{}
 	for _, row := range data.Values[1:] {
-		if cardnumber, ok := row[0].(string); !ok {
+		if cardnumber, ok := row[index["cardnumber"]].(string); !ok {
 			continue
 		} else if ok, err := regexp.Match(`^\s*[0-9]+\s*$`, []byte(cardnumber)); !ok || err != nil {
 			continue
 		}
 
-		record := make([]string, len(row))
-		for i, v := range row {
-			record[i] = strings.TrimSpace(v.(string))
+		if from, ok := row[index["from"]].(string); !ok {
+			continue
+		} else if _, err := time.ParseInLocation("2006-01-02", from, time.Local); err != nil {
+			continue
 		}
+
+		if to, ok := row[index["to"]].(string); !ok {
+			continue
+		} else if _, err := time.ParseInLocation("2006-01-02", to, time.Local); err != nil {
+			continue
+		}
+
+		record := []string{}
+		for _, h := range header {
+			k := normalise(h)
+			v := ""
+			if ix, ok := index[k]; ok {
+				v = row[ix].(string)
+			}
+
+			record = append(record, clean(v))
+		}
+
 		records = append(records, record)
 	}
 
--- acl/tsv_test.go
+++ acl/tsv_test.go
@@ -32,6 +32,31 @@ func TestMakeTSV(t *testing.T) {
 	}
 }
 
+func TestMakeTSVWithOutOfOrderColumns(t *testing.T) {
+	expected := `Card Number	From	To	Gate	Tower	Dungeon	Lair
+6001001	2020-01-01	2020-12-31	Y	N	N	Y
+6001002	2020-02-03	2020-11-30	Y	Y	N	N
+`
+
+	var f strings.Builder
+	var data = sheets.ValueRange{
+		Values: [][]interface{}{
+			[]interface{}{"Gate", "Card Number", "Tower", "To", "From", "Dungeon", "Lair"},
+			[]interface{}{"Y", "6001001", "N", "2020-12-31", "2020-01-01", "N", "Y"},
+			[]interface{}{"Y", "6001002", "Y", "2020-11-30", "2020-02-03", "N", "N"},
+		},
+	}
+
+	err := MakeTSV(&f, &data)
+	if err != nil {
+		t.Fatalf("Unexpected error returned from MakeTSV (%v)", err)
+	}
+
+	if f.String() != expected {
+		t.Errorf("Incorrect TSV\n   expected: %s\n   got:      %s\n", expected, f.String())
+	}
+}
+
 func TestMakeTSVWithEmptySheet(t *testing.T) {
 	var f strings.Builder
 	var data = sheets.ValueRange{}
@@ -99,5 +124,99 @@ func TestMakeTSVWithMissingToDate(t *testing.T) {
 	err := MakeTSV(&f, &data)
 	if err == nil {
 		t.Fatalf("Expected error return for missing 'to' column, got %v", err)
+	}
+}
+
+func TestMakeTSVWithDuplicatedColumn(t *testing.T) {
+	var f strings.Builder
+	var data = sheets.ValueRange{
+		Values: [][]interface{}{
+			[]interface{}{"Card Number", "From", "To", "Gate", "Tower", "Dungeon", "Gate"},
+			[]interface{}{"6001001", "2020-01-01", "2020-12-31", "Y", "N", "N", "Y"},
+			[]interface{}{"6001002", "2020-02-03", "2020-11-30", "Y", "Y", "N", "N"},
+		},
+	}
+
+	err := MakeTSV(&f, &data)
+	if err == nil {
+		t.Fatalf("Expected error return for duplicated column, got %v", err)
+	}
+}
+
+func TestMakeTSVWithInvalidCardNumber(t *testing.T) {
+	expected := `Card Number	From	To	Gate	Tower	Dungeon	Lair
+6001001	2020-01-01	2020-12-31	Y	N	N	Y
+6001003	2020-01-01	2020-12-31	Y	N	Y	N
+`
+
+	var f strings.Builder
+	var data = sheets.ValueRange{
+		Values: [][]interface{}{
+			[]interface{}{"Card Number", "From", "To", "Gate", "Tower", "Dungeon", "Lair"},
+			[]interface{}{"6001001", "2020-01-01", "2020-12-31", "Y", "N", "N", "Y"},
+			[]interface{}{"600100X", "2020-02-03", "2020-11-30", "Y", "Y", "N", "N"},
+			[]interface{}{"6001003", "2020-01-01", "2020-12-31", "Y", "N", "Y", "N"},
+		},
+	}
+
+	err := MakeTSV(&f, &data)
+	if err != nil {
+		t.Fatalf("Unexpected error returned from MakeTSV (%v)", err)
+	}
+
+	if f.String() != expected {
+		t.Errorf("Incorrect TSV\n   expected: %s\n   got:      %s\n", expected, f.String())
+	}
+}
+
+func TestMakeTSVWithInvalidFromDate(t *testing.T) {
+	expected := `Card Number	From	To	Gate	Tower	Dungeon	Lair
+6001001	2020-01-01	2020-12-31	Y	N	N	Y
+6001003	2020-01-01	2020-12-31	Y	N	Y	N
+`
+
+	var f strings.Builder
+	var data = sheets.ValueRange{
+		Values: [][]interface{}{
+			[]interface{}{"Card Number", "From", "To", "Gate", "Tower", "Dungeon", "Lair"},
+			[]interface{}{"6001001", "2020-01-01", "2020-12-31", "Y", "N", "N", "Y"},
+			[]interface{}{"6001002", "2020-02-0X", "2020-11-30", "Y", "Y", "N", "N"},
+			[]interface{}{"6001003", "2020-01-01", "2020-12-31", "Y", "N", "Y", "N"},
+		},
+	}
+
+	err := MakeTSV(&f, &data)
+	if err != nil {
+		t.Fatalf("Unexpected error returned from MakeTSV (%v)", err)
+	}
+
+	if f.String() != expected {
+		t.Errorf("Incorrect TSV\n   expected: %s\n   got:      %s\n", expected, f.String())
+	}
+}
+
+func TestMakeTSVWithInvalidToDate(t *testing.T) {
+	expected := `Card Number	From	To	Gate	Tower	Dungeon	Lair
+6001001	2020-01-01	2020-12-31	Y	N	N	Y
+6001003	2020-01-01	2020-12-31	Y	N	Y	N
+`
+
+	var f strings.Builder
+	var data = sheets.ValueRange{
+		Values: [][]interface{}{
+			[]interface{}{"Card Number", "From", "To", "Gate", "Tower", "Dungeon", "Lair"},
+			[]interface{}{"6001001", "2020-01-01", "2020-12-31", "Y", "N", "N", "Y"},
+			[]interface{}{"6001002", "2020-02-03", "2020-11-3X", "Y", "Y", "N", "N"},
+			[]interface{}{"6001003", "2020-01-01", "2020-12-31", "Y", "N", "Y", "N"},
+		},
+	}
+
+	err := MakeTSV(&f, &data)
+	if err != nil {
+		t.Fatalf("Unexpected error returned from MakeTSV (%v)", err)
+	}
+
+	if f.String() != expected {
+		t.Errorf("Incorrect TSV\n   expected: %s\n   got:      %s\n", expected, f.String())
 	}
 }
